6ce2c5f29c7e0e977e2f4e104a11f7f0ae6072b5
Maya Shavin <maya@cloudinary.com>
2019-03-25 19:29:40 +0200

    init

---
+++ test/test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+var assert   = require('assert');
+var testgen  = require('markdown-it-testgen');
+var path     = require('path');
+
+/*eslint-env mocha*/
+
+// Most of the rest of this is inlined from generate(), but modified
+// so we can pass in an `env` object
+function generate(fixturePath, md, env) {
+  testgen.load(fixturePath, {}, function (data) {
+    data.meta = data.meta || {};
+
+    var desc = data.meta.desc || path.relative(fixturePath, data.file);
+
+    (data.meta.skip ? describe.skip : describe)(desc, function () {
+      data.fixtures.forEach(function (fixture) {
+        it('line ' + (fixture.first.range[0] - 1), function () {
+          // add variant character after "↩", so we don't have to worry about
+          // invisible characters in tests
+          assert.strictEqual(
+            md.render(fixture.first.text, Object.assign({}, env || {})),
+            fixture.second.text.replace(/\u21a9(?!\ufe0e)/g, '\u21a9\ufe0e')
+          );
+        });
+      });
+    });
+  });
+}
+
+
+// describe('footnote.txt', function () {
+//   var md = require('markdown-it')().use(require('../'));
+
+//   // Check that defaults work correctly
+//   generate(path.join(__dirname, 'fixtures/footnote.txt'), md);
+// });
+
+// describe('custom docId in env', function () {
+//   var md = require('markdown-it')().use(require('../'));
+
+//   // Now check that using `env.documentId` works to prefix IDs
+//   generate(path.join(__dirname, 'fixtures/footnote-prefixed.txt'), md, { docId: 'test-doc-id' });
+// });
